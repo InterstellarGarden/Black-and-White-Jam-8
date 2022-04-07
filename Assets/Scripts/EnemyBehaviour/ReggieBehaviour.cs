ec2546182410b9dfa391cee442bd413fcf27c30c
mehmenmike <mehmenmike@gmail.com>
2022-04-07 23:45:51 +0100

    Add Reggie movement

--- Assets/Scripts/EnemyBehaviour/ReggieBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour/ReggieBehaviour.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class ReggieBehaviour : EnemyBehaviour
 {
-    
+    [Range(1, 10)][SerializeField]
+    private float movementSpeed;
+    [Range(0, 180)][SerializeField]
+    private int movementDirection;
+    [Range(1, 10)][SerializeField]
+    private int movementMagnitude;
+    private float displacementIncrementer;
+private Transform transform;
+    private Vector3 homePosition;
+    public void Awake(){
+        this.transform = GetComponent<Transform>();
+        this.homePosition = this.transform.position;
+        this.displacementIncrementer = 0f;
+    }
+
+    public void Update(){
+        Vector3 displacement = Vector3.left * Mathf.Sin(this.displacementIncrementer) * this.movementMagnitude;
+        displacement = Quaternion.Euler(0, this.movementDirection, 0) * displacement;
+
+        this.transform.position = this.homePosition + displacement;
+        this.displacementIncrementer += (this.movementSpeed / 100);
+
+        // prevent overflows
+        if(this.displacementIncrementer >= 360){
+            this.displacementIncrementer -= 360;
+        }
+    }
 }
