167dcf6d95281607a417025507f5ae9c3f6b374a
Steventus <runescape_dudez@hotmail.com>
2022-04-08 14:13:23 +0800

    Squashed commit of the following:
    commit 56c56006772df18fe150b6f086216c2f29ebf9bb
    Author: mehmenmike <mehmenmike@gmail.com>
    Date:   Fri Apr 8 00:43:11 2022 +0100
    
        forgot to commit
    
    commit ec2546182410b9dfa391cee442bd413fcf27c30c
    Author: mehmenmike <mehmenmike@gmail.com>
    Date:   Thu Apr 7 23:45:51 2022 +0100
    
        Add Reggie movement

--- Assets/Scripts/EnemyBehaviour/ReggieBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour/ReggieBehaviour.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class ReggieBehaviour : EnemyBehaviour
 {
-    
+    [Range(1, 10)][SerializeField]
+    private float movementSpeed = 1f;
+    [Range(0, 180)][SerializeField]
+    private int movementDirection = 0;
+    [Range(1, 10)][SerializeField]
+    private int movementMagnitude = 1;
+    private Transform transform;
+    private Vector3 homePosition;
+    public void Awake(){
+        this.transform = GetComponent<Transform>();
+        this.homePosition = this.transform.position;
+    }
+
+    public void Update(){
+        Vector3 displacement = Vector3.left * Mathf.Sin(Time.time * this.movementSpeed) * this.movementMagnitude;
+        displacement = Quaternion.Euler(0, this.movementDirection, 0) * displacement;
+
+        this.transform.position = this.homePosition + displacement;
+    }
 }
