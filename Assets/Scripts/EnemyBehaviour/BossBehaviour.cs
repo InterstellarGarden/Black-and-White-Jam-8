0db49a9094f0b1d84a6eb8e9343e8382e9f128cc
Steventus <runescape_dudez@hotmail.com>
2022-04-08 12:07:43 +0800

    Prep

--- Assets/Scripts/EnemyBehaviour/BossBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour/BossBehaviour.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class BossBehaviour : EnemyBehaviour
 {
-    private void Start()
+    protected override void Start()
     {
         FindObjectOfType<CombatManager>().isBossAlive = true;
+        base.Start();
     }
 
     public override void Death()
     {
         FindObjectOfType<CombatManager>().isBossAlive = false;
+        FindObjectOfType<CarriageManager>().UpdateIncreaseLoop();
         base.Death();   
     }
 }
