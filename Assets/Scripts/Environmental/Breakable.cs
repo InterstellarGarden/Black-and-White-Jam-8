fe7ac9935f146f0c76fcf0a8c9935d56753b7223
Shadow-DA-B0SS <102625721+Shadow-DA-B0SS@users.noreply.github.com>
2022-04-10 20:59:44 -0600

    Hazel assets (#38)
    * Added sound to DemoBarrel and DemoCrate
    
    * Adding an ass-ton of ass-ets
    
    * Safe and Piano assets
    
    * Huge push of assets+prefabs
    
    * Changes to piano asset+prefab
    
    * Update Effects_Dither.mat
    
    Co-authored-by: hazelvr <hbanceu@gmail.com>
    Co-authored-by: Steventus <56802489+steventus@users.noreply.github.com>

--- Assets/Scripts/Environmental/Breakable.cs
+++ Assets/Scripts/Environmental/Breakable.cs
@@ -13,6 +13,9 @@ public class Breakable : MonoBehaviour
 	[SerializeField]
 	private ParticleSystem poof;
 
+	[SerializeField]
+	private AudioSource breakSfx;
+
     //The force at which pieces are flung when the object is broken
     [SerializeField]
     float explosionForce = 200f;
@@ -24,38 +27,55 @@ public class Breakable : MonoBehaviour
     {
         not_broken.SetActive(true);
         broken.SetActive(false);
-    }
+
+		if (breakSfx == null)
+		{
+			breakSfx = gameObject.GetComponent<AudioSource>();
+		}
+	}
 
     public void Break()
     {
-        //If not already broken
-        if (!broken.activeSelf)
-        {
-            //Set broken to be in the same position as unbroken in case the un broken object moved before breaking
-            broken.transform.position = not_broken.transform.position;
-            broken.transform.rotation = not_broken.transform.rotation;
-
-            //Enable the broken object
-            broken.SetActive(true);
-
-            //Give every boken piece the same velocity as the unbroken object in case the object was falling while being broken
-            //Also add an explosion force
-            Rigidbody thisRb = not_broken.GetComponent<Rigidbody>();
-            foreach (Rigidbody rb in broken.GetComponentsInChildren<Rigidbody>())
-            {
-                rb.velocity = thisRb.velocity;
-                Vector3 randomVector = new Vector3(Random.Range(0,0.5f),Random.Range(0,0.5f),Random.Range(0,0.5f));
-                rb.AddExplosionForce(explosionForce, thisRb.position + randomVector, Mathf.Infinity,explosionUpwardsModifier, ForceMode.Force);
-            }
-
-			//Play particle anim
-			if (poof != null)
+		if (broken != null && not_broken != null)
+		{
+			//If not already broken
+			if (!broken.activeSelf)
 			{
-				poof.Play();
-			}
+				//Set broken to be in the same position as unbroken in case the un broken object moved before breaking
+				broken.transform.position = not_broken.transform.position + broken.transform.localPosition;
+				broken.transform.rotation = not_broken.transform.rotation;
 
-			//Get rid of the unbroken object
-			Destroy(not_broken.gameObject);
-        }
+				//Enable the broken object
+				broken.SetActive(true);
+
+				//Give every broken piece the same velocity as the unbroken object in case the object was falling while being broken
+				//Also add an explosion force
+
+				Rigidbody thisRb = not_broken.GetComponent<Rigidbody>();
+				foreach (Rigidbody rb in broken.GetComponentsInChildren<Rigidbody>())
+				{
+					rb.velocity = thisRb.velocity;
+					Vector3 randomVector = new Vector3(Random.Range(0, 0.5f), Random.Range(0, 0.5f), Random.Range(0, 0.5f));
+					rb.AddExplosionForce(explosionForce, thisRb.position + randomVector, Mathf.Infinity, explosionUpwardsModifier, ForceMode.Force);
+				}
+
+				//Get rid of the unbroken object
+				Destroy(not_broken.gameObject);
+			}
+		}
+
+		//Play particle anim
+		if (poof != null)
+		{
+			poof.Play();
+		}
+		
+		//Play sfx
+		if (breakSfx != null)
+		{
+			//Set a little variation in the break sound pitch
+			breakSfx.pitch = Random.Range(0.75f, 1.25f);
+			breakSfx.Play();
+		}
     }
 }
